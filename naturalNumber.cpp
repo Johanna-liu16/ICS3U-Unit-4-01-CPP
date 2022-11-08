4cd525c203184877043591aa4fa6204c2dc677bf
Johanna Liu <johanna.liu@mths.ca>
2022-11-08 15:42:15 +0000

    added

--- naturalNumber.cpp
+++ naturalNumber.cpp
@@ -9,27 +9,27 @@
 
 int main() {
     // this function generates a random number
-    std::string stringYear;
+    std::string stringNumber;
     int intNumber;
     int loopCounter = 0;
     int answer = 0;
 
     // input
     std::cout << "This program adds consecutive integers." << std::endl;
-    std::cout << "Enter amount of integers added: ";
+    std::cout << "Enter amount of integers to be added: ";
     std::cin >> stringNumber;
 
     // process and output
     try {
         intNumber = std::stoi(stringNumber);
-        while (loopCounter <= intNumber) {
-            std::cout << loopCounter << " time through loop." << std::endl;
+        while (loopCounter < intNumber) {
             loopCounter = loopCounter + 1;
+            answer = answer + loopCounter;
         }
+        std::cout << "The answer is " << answer << std::endl;
     } catch (std::invalid_argument) {
         std::cout << "Invalid Integer.";
     }
-
     std::cout << std::endl;
 
     std::cout << "\nDone." << std::endl;
